e74607ec5fa5f0d7771e66eb681df11b3c11a53c
Dirk Mervis <dirk.mervis@gmail.com>
2018-02-19 14:10:15 +0100

    frontend footer to bottom of the page

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { Footer } from 'react-materialize';
 
 export default () => {
-    return <Footer copyrights="Project by Peter Berkmans, Jelle Clymans, Maartje Eyskens, Dirk Mervis, Mathias Van den Bosch, Boyd van Prooije"></Footer>
+    return <Footer className="footer" copyrights="Project by Peter Berkmans, Jelle Clymans, Maartje Eyskens, Dirk Mervis, Mathias Van den Bosch, Boyd van Prooijen"></Footer>
 }
