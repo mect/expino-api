89ee9cf0191fbc335682088db159041131974fc6
Maartje Eyskens <maartje@eyskens.me>
2021-05-14 18:57:01 +0200

    Fix path logic
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- pkg/api/display/delijn.go
+++ pkg/api/display/delijn.go
@@ -1,12 +1,9 @@
 package display
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"path"
-
-	"github.com/mect/expino-api/pkg/db"
 
 	"github.com/labstack/echo/v4"
 )
@@ -15,16 +12,7 @@ import (
 const delijnHost = "https://b2cservices.delijn.be"
 
 func (h *HTTPHandler) handleDelijn(c echo.Context) error {
-	display, ok := c.Get("display").(*db.Display)
-	if !ok {
-		return c.JSON(http.StatusInternalServerError, echo.Map{"error": "error reading display data from session"})
-	}
-
-	if display.TickerRSS == "" {
-		return c.JSON(http.StatusBadRequest, echo.Map{"error": "no RSS set"})
-	}
-
-	resp, err := http.Get(fmt.Sprintln("%s%s", delijnHost, path.Join("/", c.QueryParam("path"))))
+	resp, err := http.Get(delijnHost + path.Join("/", c.QueryParam("path")))
 	if err != nil {
 		return err
 	}
